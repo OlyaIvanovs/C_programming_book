8b329badac9f1fd80eb88f864b70077d2a48028e
Olga Ivanova <olya@ivanovs.info>
2019-10-21 23:54:11 +0200

    Ex13

--- chapter1/ex13.c
+++ chapter1/ex13.c
@@ -2,13 +2,13 @@
 
 #define OUT 0
 #define IN 1
-#define NUMW 5
+#define NUMW 100
 
 /* Print a histogram of the lengths of words */
 
-main()
+int main()
 {
-    int c, lw, i, k, m, state;
+    int c, lw, i, k, m, state, max, t;
     int lengths[NUMW];
 
     i = lw = k = 0;
@@ -16,7 +16,7 @@ main()
     for (i = 0; i < NUMW; ++i)
         lengths[i] = 0;
 
-    while ((c = getchar()) != EOF) {
+    while ((c = getchar()) != EOF && k <= 100) {
         if (c == ' ' || c == '\n' || c == '\t') {
             if (state == IN) {
                 lengths[k] = lw; 
@@ -31,11 +31,31 @@ main()
 
     }
 
-    for (i = 0; i < NUMW; ++i) {
-        printf("%d ", i+1);   
+    for (i=0; i < k; ++i) {
+        printf("%d ", i + 1);   
         for (m = 0; m < lengths[i]; ++m) {
             printf("_");
         }
         printf("\n");   
     }
+
+    max = lengths[0];
+    for (i=1; i < k; ++i) {
+         if (lengths[i] > max)
+            max = lengths[i];
+    }
+
+    printf("Vertikal\n");
+    for (t=max; t > 0; --t) {
+        for (i=0; i < k; ++i) {
+            if (lengths[i] < t) {
+                printf(" ");
+            } else {
+                printf("*");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
