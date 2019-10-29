770203bbad8513d18cd88e068acc7d8b46c7dff6
Olga Ivanova <olya@ivanovs.info>
2019-10-29 21:10:56 +0100

    Ex4

---
+++ chapter2/ex4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#define MAXLINE 100
+
+/* Deletes each character in s1 that matches any character in the string s2 */
+
+void squeeze(char s1[], char s2[]);
+
+int main()
+{
+    char s1[MAXLINE] = "The staccndart library includes a portable implementation of a pseudo-random number generator";
+    char s2[MAXLINE] = "abc";
+
+    squeeze(s1, s2);
+    printf("%s", s1);
+
+}
+
+
+void squeeze(char s1[], char s2[])
+{
+    int k = 0;
+    int next_character;
+
+    for (int i = 0; s1[i] != '\0'; i++) {
+        next_character = 1;
+        for (int j = 0; s2[j] != '\0' && next_character == 1; j++) {
+            if (s1[i] == s2[j]) {  // if there is an occurence of the characters from s2 in s1, stop looping through s2
+                next_character = 0;
+            }
+        }
+        if (next_character == 1) {  // if there is no occurence of the characters from s2 in s1
+            s1[k++] = s1[i];
+        }
+    }
+
+    s1[k] = '\0';
+}
