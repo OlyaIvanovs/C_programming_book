cfee1fe7b70f879da4822288faa28de3c7c6dbd2
Olga Ivanova <olya@ivanovs.info>
2019-12-02 22:33:53 +0100

    Version of the strncpy

--- chapter5/ex5-1.c
+++ chapter5/ex5-1.c
@@ -6,10 +6,11 @@ void mystrncpy(char *, char *, int n);
 int main() 
 {
     char s[MAXLINE] = "myla ramy toto mama";
-    char t[MAXLINE] = "toto mma";
-    int num = 3;
+    char t[MAXLINE] = "tototo";
+    int num = 10;
 
-    mystrncpy(s, t, num);
+    if (num > 0)
+        mystrncpy(s, t, num);
     printf("%s", s);
 
     return 0;
@@ -19,14 +20,12 @@ int main()
 /* copies at most n characters of t to s */
 void mystrncpy(char *s, char *t, int n)
 {   
+    while (n > 0) {
+        if (*t != '\0')
+        *s++ = *t++;
+        n--;
+    }
     while (*s++ != '\0')
         ;
-    s--;
-    if (n <= 0) return;
-
-    while ((*s++ = *t++) != '\0') {
-        n--;
-        if (n == 0)
-            return;
-    }
+    *s++ = '\0';
 }
