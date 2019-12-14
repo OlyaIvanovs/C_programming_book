ca0fb43307968b887fd37a3912c67eae556713fe
Olga Ivanova <olya@ivanovs.info>
2019-12-14 22:07:19 +0100

    ex7

--- chapter5/ex7.c
+++ chapter5/ex7.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAXLINES 5000
+#define MAXLINES 1000
 #define MAXLEN 1000 /* max length of any input line */
+#define MAXTEXT 100000
 
 char *lineptr[MAXLINES]; /* pointers to text lines */
+char lines[MAXTEXT]; /* to store lines */
 
 int readlines(char *lineptr[], int nlines);
 void writelines(char *lineptr[], int nlines);
-int getline(char *, int);
-char *alloc(int);
+int mgetline(char *, int);
 void qsort(char *lineptr[], int left, int right);
 
 /* sort input lines */
@@ -17,7 +18,8 @@ int main()
 {
     int nlines;
 
-    if (nlines == readlines(lineptr, MAXLINES) >= 0) {
+    if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
+        // printf("lineptr 0 %s", lineptr[0]);
         qsort(lineptr, 0, nlines - 1);
         writelines(lineptr, nlines);
         return 0;
@@ -32,17 +34,22 @@ int main()
 int readlines(char *lineptr[], int maxlines)
 {
     int len, nlines;
-    char *p, line[MAXLEN];
+    char line[MAXLEN];
+    char *current = &lines[0];
 
     nlines = 0;
-    while ((len = getline(line, MAXLEN)) > 0)
-        if (nlines >= maxlines || (p = alloc(len)) == NULL)
+    while ((len = mgetline(line, MAXLEN)) > 0) {
+        // if (nlines >= maxlines || (p = alloc(len)) == NULL)
+        if (nlines >= maxlines || (lines - &lines[0]+ len) > MAXTEXT)
             return -1;
-        else {
-            line[len-1] = '\0'; /* delete newline */
-            strcpy(p, line);
-            lineptr[nlines++] = p;
+        else {   
+        lineptr[nlines++] = current;
+        strncpy(current, line, len);
+        current += len;
+        *current = '\0';
+        current++;
         }
+    }
     return nlines;
 }
 
@@ -50,7 +57,7 @@ int readlines(char *lineptr[], int maxlines)
 void writelines(char *lineptr[], int nlines)
 {
     while (nlines-- > 0)
-        printf("%s\n", *lineptr++);
+        printf("%s", *lineptr++);
 }
 
 void qsort(char *v[], int left, int right)
@@ -80,15 +87,16 @@ void swap(char *v[], int i, int j)
     v[j] = temp;
 }
 
-int getline(char *s, int lim)
+int mgetline(char *s, int lim)
 {
     int c, i;
+    char *t = s;
 
-    // for (i = 0; i < lim -1 && (c=getchar()) != EOF && c != '\n'; ++i)
-    //     *s++ = c;
-    // if (c == '\n')
-    //     *s++ = c;
-    // *s = '\n';
+    while (lim-- > 0 && (c=getchar()) != EOF && c != '\n')
+        *s++ = c;
+    if (c == '\n')
+        *s++ = c;
+    *s = '\0';
 
-    return i;
+    return s-t;
 }
